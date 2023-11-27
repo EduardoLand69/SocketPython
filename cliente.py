214faf9c635883b15eb6964030ce8fc15b60b422
Eduardo Landa <mendezlandae@gmail.com>
2023-11-27 16:47:06 -0600

    cliente

---
+++ cliente.py
@@ -0,0 +1,20 @@
+import socket
+
+#Declaramos variables de conexion
+host = '127.0.0.1'
+port = 8080
+
+with socket.socket((socket.AF_INET, socket.SOCK_STREAM) as client_socket:)
+    cliente.connect((host,port))
+    
+    print("Conexión exitosa")
+    
+    msj = "Hola, ¿En qué puedo ayudarle?"
+    cliente.sendall(msj.encode('utf-8'))
+    
+    
+    #Aquí le pones lo que lleve del socket
+    data = socket.recv()
+    print(f"Dice {data.decode('uft-8')}")
+    
+    print("Cerrando cliente")
