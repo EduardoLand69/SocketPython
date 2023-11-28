0fd4d6aecf4684fc8721b9b2a0328b60020edbbb
Luis Mario Cervantes Suarez <z21015977@estudiantes.uv.mx>
2023-11-27 18:59:13 -0600

    Plantilla de cliente

--- cliente.py
+++ cliente.py
@@ -2,19 +2,36 @@ import socket
 
 #Declaramos variables de conexion
 host = '127.0.0.1'
-port = 8080
+port = 3103
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente:
     cliente.connect((host,port))
     
     print("Conexión exitosa")
     
-    msj = "Hola, ¿En qué puedo ayudarle?"
-    cliente.sendall(msj.encode('utf-8'))
+    ####Enviar una respuesta al servidor
+    #El servidor esta pidiendo una ruta
+    msg_servidor = cliente.recv(1024)
+    #capturar una respuesta
+    respuesta_cliente = input(msg_servidor.decode('utf-8'))
+    #enviar el mensaje al servidor
+    cliente.sendall(respuesta_cliente.encode('utf-8'))
+    
+    ####Enviar una respuesta al servidor de si o no
+    #El servidor esta pidiendo una ruta
+    msg_servidor = cliente.recv(1024)
+    #capturar una respuesta
+    respuesta_cliente = input(msg_servidor.decode('utf-8'))
+    #enviar el mensaje al servidor
+    cliente.sendall(respuesta_cliente.encode('utf-8'))
     
     
-    #Aquí le pones lo que lleve del socket
-    data = socket.recv(1024)
-    print(f"Dice {data.decode('uft-8')}")
+    ####Enviar una respuesta al servidor para buscar el archivo
+    #El servidor esta pidiendo una ruta
+    msg_servidor = cliente.recv(1024)
+    #capturar una respuesta
+    respuesta_cliente = input(msg_servidor.decode('utf-8'))
+    #enviar el mensaje al servidor
+    cliente.sendall(respuesta_cliente.encode('utf-8'))
     
     print("Cerrando cliente")
