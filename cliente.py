d093b5bfbcd7117a1e9a013387b78ec36bbedf94
Eduardo Landa <mendezlandae@gmail.com>
2023-11-30 10:54:20 -0600

    IF implementados

--- cliente.py
+++ cliente.py
@@ -23,39 +23,43 @@ with sk.socket(sk.AF_INET, sk.SOCK_STREAM) as cliente:
     #El servidor esta pidiendo una repsuesta
     msg_servidor = cliente.recv(1024) #Recibibr verificación del código
     if msg_servidor.decode('utf-8') == "ok": #El directorio si existe
+        if respuesta_cliente == "y" or respuesta_cliente == "Y": #¿Quieres crear el archivo?
+            msg_servidor = cliente.recv(1024)
+            respuesta_cliente = input(msg_servidor.decode('utf-8'))
+            cliente.sendall(respuesta_cliente.encode('utf-8'))  
+            if respuesta_cliente == "y" or respuesta_cliente == "Y": #¿Quieres sobreescribir el archivo?
+                msg_servidor = cliente.recv(1024)
+                respuesta_cliente = input(msg_servidor.decode('utf-8'))
+                cliente.sendall(respuesta_cliente.encode('utf-8'))
+            elif respuesta_cliente == "n" or respuesta_cliente =="N":
+                print("El cliente se cerrará")
+                tm.sleep(3)
+                exit
+        elif respuesta_cliente == "n" or respuesta_cliente == "N":
+            print("El cliente se cerrara")
+            tm.sleep(3)
+            exit
         pass
     if msg_servidor.decode('utf-8') == "no": #El directorio no existe
-        pass
-    
-    ####Enviar una respuesta al servidor
-    #El servidor esta preguntando si se quiere crear el direcotorio o no
-    msg_servidor = cliente.recv(1024)
-    #capturar una respuesta
-    respuesta_cliente = input(msg_servidor.decode('utf-8'))
-    #enviar el mensaje al servidor
-    cliente.sendall(respuesta_cliente.encode('utf-8'))
-    if respuesta_cliente == "y": #¿Quieres crear el archivo? en caso de que la ruta exista
-        #El servidor esta pidiendo una ruta
-        msg_servidor = cliente.recv(1024)
-        #capturar una respuesta
-        respuesta_cliente = input(msg_servidor.decode('utf-8'))
-        #enviar el mensaje al servidor
-        cliente.sendall(respuesta_cliente.encode('utf-8'))
-        if respuesta_cliente == "y":#¿Quieres crear el directorio? en caso de que NO exista la ruta
+        if respuesta_cliente == "y" or respuesta_cliente == "Y": #¿Quieres crear el directorio?
             msg_servidor = cliente.recv(1024)
             respuesta_cliente = input(msg_servidor.decode('utf-8'))
             cliente.sendall(respuesta_cliente.encode('utf-8'))
-        elif respuesta_cliente == "n":
+            if respuesta_cliente == "y" or respuesta_cliente == "Y": #¿Quieres crear el archivo?
+                msg_servidor = cliente.recv(1024)
+                respuesta_cliente = input(msg_servidor.decode('utf-8'))
+                cliente.sendall(respuesta_cliente.encode('utf-8'))
+            elif respuesta_cliente == "n" or respuesta_cliente == "N":
+                print("El cliente se cerrará")
+                tm.sleep(3)
+                exit
+        elif respuesta_cliente == "n" or respuesta_cliente == "N":
             print("El cliente se cerrará")
             tm.sleep(3)
             exit
-            if respuesta_cliente == "y": #¿Quieres crear el archivo? en caso de que se cree el directorio nuevo
-                
-    elif respuesta_cliente == "n":
-        print("El cliente se cerrará")
-        tm.sleep(3)
-        exit
+        pass
     
+
     ####Enviar una respuesta al servidor
     #El servidor esta pidiendo una archivo
     msg_servidor = cliente.recv(1024)
